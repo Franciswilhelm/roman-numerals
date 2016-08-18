6f0fbd9f6e3ae07650c4a1a9dfd9819f4e670e66
David Ayala and Francis Prus <francis@>
2016-08-18 09:29:28 -0700

    create initial htlm page.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var numerals = ["I", "V", "X", "L", "C", "D", "M"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+  });
+});
